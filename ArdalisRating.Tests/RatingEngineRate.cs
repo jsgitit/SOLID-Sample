240435661cfbcab5e47e778b27ef0be6b93352ed
Jon Smart <jsmartware@gmail.com>
2020-10-02 14:04:31 -0700

    Added Persistence abstraction for PolicySource and unit tests

--- ArdalisRating.Tests/RatingEngineRate.cs
+++ ArdalisRating.Tests/RatingEngineRate.cs
@@ -1,5 +1,4 @@
 using Newtonsoft.Json;
-using System;
 using System.IO;
 using Xunit;
 
@@ -7,24 +6,24 @@ namespace ArdalisRating.Tests
 {
     public class RatingEngineRate
     {
-[Fact]
-public void ReturnsRatingOf10000For200000LandPolicy()
-{
-    var policy = new Policy
-    {
-        Type = PolicyType.Land,
-        BondAmount = 200000,
-        Valuation = 200000
-    };
-    string json = JsonConvert.SerializeObject(policy);
-    File.WriteAllText("policy.json", json);
+        [Fact]
+        public void ReturnsRatingOf10000For200000LandPolicy()
+        {
+            var policy = new Policy
+            {
+                Type = PolicyType.Land,
+                BondAmount = 200000,
+                Valuation = 200000
+            };
+            string json = JsonConvert.SerializeObject(policy);
+            File.WriteAllText("policy.json", json);
 
-    var engine = new RatingEngine();
-    engine.Rate();
-    var result = engine.Rating;
+            var engine = new RatingEngine();
+            engine.Rate();
+            var result = engine.Rating;
 
-    Assert.Equal(10000, result);
-}
+            Assert.Equal(10000, result);
+        }
 
         [Fact]
         public void ReturnsRatingOf0For200000BondOn260000LandPolicy()
@@ -43,6 +42,28 @@ public void ReturnsRatingOf10000For200000LandPolicy()
             var result = engine.Rating;
 
             Assert.Equal(0, result);
+        }
+        [Fact]
+
+        public void CanCreateLoggerInstance()
+        {
+            var logger = new ConsoleLogger();
+
+            logger.Log("I'm able to log to the console!");
+
+            Assert.NotNull(logger);
+        }
+
+        [Fact]
+        public void CanReadFilePolicy()
+        {
+            var policySource = new FilePolicySource();
+
+            var policy = policySource.GetPolicyFromSource();
+
+            Assert.NotNull(policy);
+            Assert.NotEmpty(policy);
+
         }
     }
 }
